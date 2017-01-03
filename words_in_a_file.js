eb06ee1cb15c910394e14490badeca5670502305
Syanmil <syanmil@yahoo.com>
2017-01-03 13:01:20 +0700

    initialize readfile done, need logic in it

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -1,3 +1,7 @@
 var sys = require('sys');
-
+var fs = require('fs')
 // actual conversion code starts here
+fs.readFile('source.txt', "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
