90e39cfe3ad3a094c3233fea1e8434bda7801dc7
Syanmil <syanmil@yahoo.com>
2017-01-03 13:27:33 +0700

    turn data into array

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -1,7 +1,9 @@
 var sys = require('sys');
 var fs = require('fs')
 // actual conversion code starts here
+var arrData = [];
 fs.readFile('source.txt', "utf8", (err, data) => {
   if (err) throw err;
+  data = data.split(" ")
   console.log(data);
 });
