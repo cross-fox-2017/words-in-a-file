f587cc3a551b545c6a17d93267091d0c294d79af
Dale <dellryuzi@gmail.com>
2017-01-04 10:46:02 +0700

    masih menghitung perbaris

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -1,3 +1,16 @@
-var sys = require('sys');
+var fs = require('fs');
 
-// actual conversion code starts here
+fs.readFile('test.txt','utf8', function(err,arr){
+
+  let data = arr.split("\r\n").join("");
+  //let data = data1.split(" ");
+
+  var table ={};
+  for(let i=0; i< data.length;i++){
+var words = data[i];
+table[words] === undefined ? table[words] = 1: table[words] += 1;
+
+  }
+  console.log(table);
+
+});
